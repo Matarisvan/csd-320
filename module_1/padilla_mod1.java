56bd312b811d036ed575349cf9fe677edd0b405a
LuisMa <luismilan21@gmail.com>
2022-10-24 15:55:32 -0400

    mod1 submission ready

---
+++ module_1/padilla_mod1.java
@@ -0,0 +1,34 @@
+/*
+Luis Padilla
+10/24/22
+CSD320 Module 1 Assignment
+*/
+
+public class padilla_mod1 {
+    public static void main(String[] args) {
+        System.out.println("For Mod 1 we were tasked to display 2 different formulas");
+        System.out.println();
+        System.out.println("This first one is:");
+        System.out.println(" 4 x ( 1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0 )");
+        System.out.print("The answer for this one is: ");
+        float eq1 = 4 * ( 1.0f - 1.0f / 3.0f + 1.0f / 5.0f - 1.0f / 7.0f + 1.0f / 9.0f - 1.0f / 11.0f + 1.0f / 13.0f );
+        System.out.println(eq1);
+        System.out.println();
+        System.out.println("The second equation is:");
+        System.out.println("4 x ( 1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11 + 1 / 13 )");
+        System.out.print("The answer for this one is: ");
+        int eq2 = 4 * ( 1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11 + 1 / 13 );
+        System.out.println(eq2);
+        System.out.println();
+    }
+}
+
+/*
+The difference between the two equations is that the first one (eq1) uses the "float" type of variable while the second (eq2) uses the "int" type of variable.
+With eq1 we get the exact answer that is requested from the formula. This is because the "float" variable allows us to have decimal places after the whole number.
+    This gets us an exact number without having to round down.
+With eq2 we use the "int" variable which removes the ability to have decimals thus causing the numbers being manipulated to round down. With this rounding,
+    the numbers are now not the exact numbers that we need so the equation will not be less accurate.
+Using float is a better method for solving equations since it allows for decimals to exist which gives you the accurate answer you are looking for
+    It is a bit more tedious since you have to add "f" after each float variable but it is the more accurate response.
+ */
