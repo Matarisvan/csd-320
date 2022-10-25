e9369ecc0a08540d34fa78fc51010435216cd223
LuisMa <luismilan21@gmail.com>
2022-10-24 20:22:51 -0400

    init_assignment

--- module_2/padilla_mod2.java
+++ module_2/padilla_mod2.java
@@ -2,4 +2,18 @@
 Luis Padilla
 10/24/22
 CSD320 Module 2 Assignment
+
+Write a program that calculates the energy needed to heat water from an initial temperature to a final temperature.
+Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperature of the water.
+The formula to use for this program is:
+Q = waterMass ( finalTemperature – initialTemperature ) x 4184
+waterMass is water weight in kilograms
+finalTemperature and initialTemperature are temperatures in Celsius
+Q is the results in Joules
 */
+
+public class padilla_mod2 {
+    public static void main(String args) {
+        System.out.println("");
+    }
+}
