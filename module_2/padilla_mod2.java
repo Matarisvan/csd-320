25fbe5d64f9bb53c0853d11e9b5346a74dd4957c
LuisMa <lupadilla@my365.bellevue.edu>
2022-10-25 13:57:08 -0400

    program complete along with comments

--- module_2/padilla_mod2.java
+++ module_2/padilla_mod2.java
@@ -12,29 +12,37 @@ finalTemperature and initialTemperature are temperatures in Celsius
 Q is the results in Joules
 */
 
+// import Scanner function for user input
 import java.util.Scanner;
 
 public class padilla_mod2{
     public static void main(String[] args) {
         try (Scanner myObj = new Scanner(System.in)) {
-            System.out.println("Welcome to the Energy Calculator\nWe will be calculating how much Energy in Joules it takes to heat up your watery needs!");
-            System.out.println("We need 3 things from you:\nWater in Kilograms\nStarting Temperature in Celsius\nDesired Temperature in Celsius");
             
-            System.out.println("How much water are you heating up?");
+            // Welcome Message to inform user of program's function
+            System.out.println("\nWelcome to the Energy Calculator!\nWe will be calculating how much energy in Joules it takes to heat up your watery needs!");
+            
+            // Tells user what information is needed
+            System.out.println("\nWe need 3 things from you:\n1) Water in Kilograms\n2) Starting Temperature in Celsius\n3) Desired Temperature in Celsius");
+            
+            // get user input for water in kg
+            System.out.println("\nHow much water are you heating up?");
             float waterMass = myObj.nextFloat();
             
+            // get user input for inital temp in Celsius
             System.out.println("What is the water's current temperature?");
             float initTemp = myObj.nextFloat();
 
+            // get user input for desired temp in Celsius
             System.out.println("How warm do you need your water to be?");
             float finalTemp = myObj.nextFloat();
 
-            System.out.println(waterMass);
-            System.out.println(initTemp);
-            System.out.println(finalTemp);
+            // equation to calculate energy needed in Joules
+            float Q = waterMass*(finalTemp - initTemp) * 4184;
+            
+            // inform user how much energy is required for their specific situation
+            System.out.println("\nIt takes " + Q + " Joules to satisfy your demand.\n");
+
         }
-
-        //float Q = waterMass (finalTemp - initTemp) * 4184/* 
-
     }
 }
