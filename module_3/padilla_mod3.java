7f6f33d7c96090074b9a4e6adbaf4fcea06675ad
LuisMa <luismilan21@gmail.com>
2022-10-26 15:01:14 -0400

    init_assignment

---
+++ module_3/padilla_mod3.java
@@ -0,0 +1,20 @@
+/*
+Luis Padilla
+10/25/22
+CSD320 Module 3 Assignment
+
+Write a program that is similar to the popular game titled “Rock-Paper-Scissors.”
+The program randomly generates the number of 1, 2, or 3 with 1 being Rock, 2 being Paper, and 3 being Scissors.
+Then, the program is to prompt the user to enter a value of 1, 2, or 3.
+Next, the program displays a clear readable message displaying both the computer’s selection, the user’s selection, and the results.
+*/
+
+
+package csd_320.module_3;
+
+public class padilla_mod3 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+
+    }
+}
