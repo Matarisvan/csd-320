bf74fc5a65954abb688aa64d4cf6f9957aa9beac
LuisMa <luismilan21@gmail.com>
2022-11-11 14:22:04 -0500

    init_assignment

---
+++ module_6/padilla_mod6.java
@@ -0,0 +1,30 @@
+/*
+ Luis Padilla
+ 11/11/22
+ CSD320 Module 6 Assigment
+
+ Write a program that uses nested for loops to output the following with the shown display format:
+                                   1               @
+                                 1 2 1             @
+                               1 2 4 2 1           @
+                             1 2 4 8 4 2 1         @
+                          1 2 4 8 16 8 4 2 1       @
+                       1 2 4 8 16 32 16 8 4 2 1    @
+                    1 2 4 8 16 32 64 32 16 8 4 2 1 @
+ */
+
+package module_6;
+
+import java.util.Scanner;
+
+public class padilla_mod6 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+
+            // Welcome Message
+            System.out.println("\nWelcome to Module 6!");
+        }
+        
+    }
+    
+}
