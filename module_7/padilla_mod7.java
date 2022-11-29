bb51edb18b9c1e71943498a0794d2be24000fd69
LuisMa <luismilan21@gmail.com>
2022-11-29 12:55:17 -0500

    added try catch block in password method

--- module_7/padilla_mod7.java
+++ module_7/padilla_mod7.java
@@ -17,11 +17,20 @@ A password must contain at least one lowercase character.
 
 package module_7;
 
+import java.util.Scanner;
+
 public class padilla_mod7 {
     
     // Defining the Password Method
     static void checkPassword(String password) {
-        
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.println("Please enter your password:\t");
+            String pass = input.nextLine();
+            System.out.println("You chose \"" + pass + "\" as your password");
+        }
+        catch (Exception e) {
+            System.out.println("Oh no. Something has gone wrong :o\n");
+        }
     }
     
     // Main Program
@@ -30,6 +39,8 @@ public class padilla_mod7 {
         // Welcome Message
         System.out.println("\nHello and Welcome to Module 7!\nToday we will be creating and testing a password!\n");
         System.out.println("Your password must:\nBe at least 8 characters long\nContain at least 1 letter and 1 number\nContain at least 1 uppercase letter and 1 lowercase letter\n");
+
+        checkPassword(null);
     }
 
 }
