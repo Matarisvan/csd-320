40f0ef66c60210d900368e3fe716fbabcf23d8dd
LuisMa <lupadilla@my365.bellevue.edu>
2022-11-29 11:51:41 -0500

    init_assignment

---
+++ module_7/padilla_mod7.java
@@ -0,0 +1,25 @@
+/*
+Luis Padilla
+11/28/22
+CSD320 Module 7 Assignment
+
+Some Websites impose certain rules for passwords.
+Write a method that checks whether a user's input String is a valid password.
+Suppose the password rules are as follows:
+
+A password must have at least eight characters.
+A password must contain both letters and digits.
+A password must contain at least one uppercase character.
+A password must contain at least one lowercase character.
+
+*/
+
+
+package module_7;
+
+public class padilla_mod7 {
+    static void checkPassword(String password) {
+        
+    }
+    
+}
