a6d089e838dac26638b0cbb03c4bb99aa907f626
LuisMa <luismilan21@gmail.com>
2022-11-29 12:45:36 -0500

    added password requirements message

--- module_7/padilla_mod7.java
+++ module_7/padilla_mod7.java
@@ -18,8 +18,18 @@ A password must contain at least one lowercase character.
 package module_7;
 
 public class padilla_mod7 {
+    
+    // Defining the Password Method
     static void checkPassword(String password) {
         
     }
     
+    // Main Program
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("\nHello and Welcome to Module 7!\nToday we will be creating and testing a password!\n");
+        System.out.println("Your password must:\nBe at least 8 characters long\nContain at least 1 letter and 1 number\nContain at least 1 uppercase letter and 1 lowercase letter\n");
+    }
+
 }
