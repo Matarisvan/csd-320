13a527029846f3cfac2f58da2bfaa7291a2b0a24
LuisMa <luismilan21@gmail.com>
2022-11-30 23:12:15 -0500

    added 3/4 requirements

--- module_7/padilla_mod7.java
+++ module_7/padilla_mod7.java
@@ -22,17 +22,91 @@ import java.util.Scanner;
 public class padilla_mod7 {
     
     // Defining the Password Method
-    static void checkPassword(String password) {
+    static void getPassword(String password) {
         try(Scanner input = new Scanner(System.in)) {
             System.out.println("Please enter your password:\t");
-            String pass = input.nextLine();
-            System.out.println("You chose \"" + pass + "\" as your password");
-        }
-        catch (Exception e) {
+            
+            // Define variables
+            int passLoop = 0;
+            int passCheck;
+            int passNum;
+
+            // Password Loop
+            do {
+                passCheck = 0;
+                
+                // Get password from user
+                String pass = input.nextLine();
+                System.out.println("You chose \"" + pass + "\" as your password");
+                
+                // Checks length
+                if (pass.length() > 7) {
+                    System.out.println("\nlong enough");
+                    passCheck++;
+                } else {
+                    System.out.println("\nPassword too short");
+                }
+
+                // Checks Lowercase
+                String passU = pass.toUpperCase();
+                if (pass.equals(passU)) {
+                    System.out.println("You need a lowercase letter");
+                } else {
+                    System.out.println("lowercase detected");
+                    passCheck++;
+                }                
+
+                // Checks Uppercase
+                String passL = pass.toLowerCase();
+                if (pass.equals(passL)) {
+                    System.out.println("you need an uppercase letter");
+                } else {
+                    System.out.println("uppercase detected");
+                    passCheck++;
+                }
+
+                // Checks digit
+                passNum = 0;
+                char[] passD = pass.toCharArray();
+                do {
+                    if (pass.contains("0")){
+                        System.out.println("number detected");
+                        passNum++;
+                        break;
+                    } else if (pass.contains("1")) {
+                        System.out.println("number detected");
+                        passNum++;
+                        break;
+                    } else if (pass.contains("2")) {
+                        System.out.println("number detected");
+                        passNum++;
+                        break;
+                    } else {
+                        System.out.println("No number detected");
+                        passNum++;
+                        break;
+                    }
+                } while (passNum == 0);
+
+                System.out.println("\nYour password has passed " + passCheck + " out of 4 requirements");
+
+
+
+
+            } while (passLoop == 0);
+
+        } catch (Exception e) {
             System.out.println("Oh no. Something has gone wrong :o\n");
         }
+
+
+           
+
+        
+
+        
     }
-    
+
     // Main Program
     public static void main(String[] args) {
 
@@ -40,7 +114,9 @@ public class padilla_mod7 {
         System.out.println("\nHello and Welcome to Module 7!\nToday we will be creating and testing a password!\n");
         System.out.println("Your password must:\nBe at least 8 characters long\nContain at least 1 letter and 1 number\nContain at least 1 uppercase letter and 1 lowercase letter\n");
 
-        checkPassword(null);
+        getPassword(null);
+        
+        System.out.println("\nthank you\n");
     }
-
+    
 }
