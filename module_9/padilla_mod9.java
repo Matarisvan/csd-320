c3de43aefd5aaeac692244c20372f0f686146c93
LuisMa <lupadilla@my365.bellevue.edu>
2022-12-13 13:06:06 -0500

    added random array method for 20 ints

--- module_9/padilla_mod9.java
+++ module_9/padilla_mod9.java
@@ -18,5 +18,28 @@ Display this information in a readable display making the data easy to understan
 package module_9;
 
 public class padilla_mod9 {
-    
+    public static void main(String[] args){
+
+        //Welcome Message
+        System.out.println("\nHello and welcome to module 9!\nToday we will be learning about Arrays and getting 4 different attributes from the array:\nMin, Max, Sum, and Average\n");
+        
+        // Array method
+        ranArray(0);
+
+        
+        
+    }
+
+    // Create and fill array method
+    public static int[] ranArray(int x) {
+        // Fill array with 20 random intergers between 0 - 100
+        int[] myArray = new int[20];
+        for (int i = 0; i < myArray.length; i++){
+            myArray[i] = (int)(Math.random()*100);
+            System.out.println(myArray[i]);            
+        }
+        System.out.println("\n" + myArray.length + " = Array length");
+        return myArray;
+    }
+
 }
