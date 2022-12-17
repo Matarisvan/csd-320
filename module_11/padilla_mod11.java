1b6a6fcb66d6fbbc3bb39e0774eb71233aacd0c2
LuisMa <luismilan21@gmail.com>
2022-12-17 01:04:31 -0500

    init_assignment

---
+++ module_11/padilla_mod11.java
@@ -0,0 +1,47 @@
+/*
+Luis Padilla
+12/16/22
+CSD320 Module 11 Assignment
+
+Write methods using the following headers that returns the location of the largest element in the array passed to the 
+method returning a one-dimensional array that contains two location elements.
+public static int [] locateLargest (double [][] arrayParam)
+public static int [] locateLargest (int [][] arrayParam)
+
+Then, write methods using the following headers that returns the location of the smallest element in the array passed
+to the method returning a one-dimensional array that contains two location elements.
+public static int [] locateSmallest (double [][] arrayParam)
+public static int [] locateSmallest (int [][] arrayParam)
+
+*/
+
+
+
+
+package module_11;
+
+public class padilla_mod11 {
+    public static void main(String[] args){
+
+
+
+
+    }
+
+
+    public static int[] locateLargest(double [][] arrayParam){
+        return locateLargest(arrayParam);
+    }
+
+    public static int[] locateLargest(int [][] arrayParam){
+        return locateLargest(arrayParam);
+    }
+
+    public static int[] locateSmallest(double [][] arrayParam){
+        return locateSmallest(arrayParam);
+    }
+
+    public static int[] locateSmallest(int [][] arrayParam){
+        return locateSmallest(arrayParam);
+    }
+}
