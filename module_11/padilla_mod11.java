c3ac6670c7fcad9869a105c79fb4ca512a6f5811
LuisMa <luismilan21@gmail.com>
2022-12-17 07:03:10 -0500

    added double locateSmallest

--- module_11/padilla_mod11.java
+++ module_11/padilla_mod11.java
@@ -29,8 +29,8 @@ public class padilla_mod11 {
         Scanner input = new Scanner(System.in);
 
         // Welcome Message
-        System.out.println("\nHello and Welcome to Module 11!\nToday we will be locating the largest element within a 4x4 array");
-
+        System.out.println("\nHello and Welcome to Module 11!\nToday we will be locating the largest element within a 4x4 array containing randomly generated numbers between 1-64");
+        
         // Program Loop Variables
         int rerun = 0;
         String exit;
@@ -41,29 +41,31 @@ public class padilla_mod11 {
             // Main Variables
             double [][] doubleArray = new double[4][4];
             int [][] intArray = new int[4][4];
-
-            // Array Creation
-                // Double Array
-                for (int i = 0; i < doubleArray.length; i++){
-                    for (int j = 0; j < doubleArray.length; j++) {
-                        doubleArray[i][j] = (double)(Math.random()*65);
-                        System.out.print("[" + i + "][" + j + "] = " + doubleArray[i][j] + " ");
-                    }
-                    System.out.println(" ");
+            
+            // Double Array
+            System.out.println("\nDouble Array:");
+            for (int i = 0; i < doubleArray.length; i++){
+                for (int j = 0; j < doubleArray.length; j++) {
+                    doubleArray[i][j] = (double)(Math.random()*65);
+                    System.out.print("[" + i + "][" + j + "] = " + doubleArray[i][j] + " ");
                 }
                 System.out.println(" ");
+            }
+            System.out.println(" ");
 
-                locateLargest(doubleArray);
+            // Largest and Smallest overload methods for double
+            locateLargest(doubleArray);
+            locateSmallest(doubleArray);
 
-                // Int Array
-                for (int i = 0; i < intArray.length; i++){
-                    for (int j = 0; j < intArray.length; j++) {
-                        intArray[i][j] = (int)(Math.random()*65);
-                        System.out.print("[" + i + "][" + j + "] = " + intArray[i][j] + " ");
-                    }
-                    System.out.println(" ");
+            // Int Array
+            for (int i = 0; i < intArray.length; i++){
+                for (int j = 0; j < intArray.length; j++) {
+                    intArray[i][j] = (int)(Math.random()*65);
+                    System.out.print("[" + i + "][" + j + "] = " + intArray[i][j] + " ");
                 }
                 System.out.println(" ");
+            }
+            System.out.println(" ");
                 
                 
                 
@@ -113,7 +115,21 @@ public class padilla_mod11 {
     }
 
     public static int[] locateSmallest(double [][] arrayParam){
-        return locateSmallest(arrayParam);
+        int[] dMin = new int[2];
+        double test = 65; 
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam.length; j++) {            
+                if (arrayParam[i][j] < test) {
+                    test = arrayParam[i][j];
+                    dMin [0] = i;
+                    dMin [1] = j;
+                }                
+            }
+        }
+        System.out.println("The lowest value in the array is " + test);
+        System.out.println("You can find it at location [" + dMin[0] + "][" + dMin[1] + "]\n" );
+        return dMin;
     }
 
     public static int[] locateSmallest(int [][] arrayParam){
